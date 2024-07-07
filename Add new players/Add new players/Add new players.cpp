cd79fafb68aa9c3fdb7649a196930e2875bb50c6
nadishwara <nadishwaragimhana@gmail.com>
2024-07-07 22:09:34 +0530

    Add files via upload

---
+++ Add new players/Add new players/Add new players.cpp
@@ -0,0 +1,14 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string playerName;
+    cout<< "Enter player Name: ";
+    cin >> playerName;
+    cout << "New player name is: "<< playerName<< endl;
+}
+
+
